a4b8c28715849cfa7fa1bd30470e1771aa9a640a
Linnea <miebaekgaard@icloud.com>
2025-09-09 12:44:58 +0200

    Theme Switcher - en alternativ måde i js

--- themeswitcher/index.js
+++ themeswitcher/index.js
@@ -1,7 +1,16 @@
 "use strict";
 
-const themeSwitcher = document.querySelector("#theme-switcher");
+// const themeSwitcher = document.querySelector("#theme-switcher");
 
-themeSwitcher.addEventListener("change", function () {
-  document.body.dataset.theme = this.value;
+// themeSwitcher.addEventListener("change", function () {
+//   document.body.dataset.theme = this.value;
+// });
+
+// Kan også laves således:
+
+const bodyElm = document.querySelector("body");
+
+document.querySelector("#theme-switcher").addEventListener("change", (evt) => {
+  console.log(evt.target.value);
+  bodyElm.dataset.theme = evt.target.value;
 });
